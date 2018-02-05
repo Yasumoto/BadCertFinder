2d14abd0710c1b7e2f1a499a232eb88b73a57da7
Joe Smith <yasumoto7@gmail.com>
2018-02-05 10:47:25 -0800

    Cleaner output

--- Sources/BadCertFinder/main.swift
+++ Sources/BadCertFinder/main.swift
@@ -8,7 +8,7 @@ if (CommandLine.arguments.count != 2) {
     exit(1)
 }
 
-var expiring = Set<String>()
+var expiring = [String:Certificate]()
 let infos = enumerateCertificates(
     baseDirectory: CommandLine.arguments[1])
 let formatter = DateFormatter()
@@ -16,19 +16,19 @@ guard let soon =  Calendar.current.date(byAdding: .month, value: 1, to: Date()) 
 for (pathName, info) in infos {
     if let expirationDate = info.notAfter {
         if expirationDate < soon {
-            expiring.insert(pathName)
+            expiring[pathName] = info
         }
     }
 }
 
 let badIssuers = ["startcom", "startssl", "geotrust"]
-var reissues = Set<String>()
+var reissues = [String:Certificate]()
 for (pathName, info) in infos {
     if let issuer = info.issuer {
         if let issuerCN = issuer[.commonName] {
             for badIssuer in badIssuers {
                 if issuerCN.lowercased().contains(badIssuer) {
-                    reissues.insert(pathName)
+                    reissues[pathName] = info
                 }
             }
         }
@@ -36,19 +36,26 @@ for (pathName, info) in infos {
         for issuer in issuers {
             for badIssuer in badIssuers {
                 if issuer.lowercased().contains(badIssuer) {
-                    reissues.insert(pathName)
+                    reissues[pathName] = info
                 }
             }
         }
     }
 }
 
-print("These certs expiring soon:")
-for path in expiring {
-    print(path)
+if expiring.count > 0 {
+    print("These certs expiring soon:")
+    for (path, info) in expiring {
+        print(path)
+    }
 }
 
-print("These certs should be replaced before their CA is distrusted:")
-for path in reissues {
-    print(path)
+if reissues.count > 0 {
+    print("These certs should be replaced before their CA is distrusted:")
+    for (path, info) in reissues {
+        print(path)
+        if let names = info.alternateNames {
+            print("\t\(names.joined(separator: ", "))\n")
+        }
+    }
 }
